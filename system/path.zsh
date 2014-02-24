3e4f41c6dc44f5f43afa89607cfcc60d23ee51ab
Julien Marcil <julien@marcil.com>
2014-02-24 16:24:53 -0500

    Fixed path

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,4 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+#export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+#export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export PATH="/usr/local/bin:$PATH"
+export MANPATH="/usr/local/man:$MANPATH"
