f7a9189ce114e4fe434e0fbeda3be5a62c18f738
Julien Marcil <julien@marcil.com>
2019-08-29 15:18:31 -0400

    update java version

--- java/java.zsh
+++ java/java.zsh
@@ -1,2 +1,2 @@
-export JAVA_HOME=`/usr/libexec/java_home -v 1.8e`
-## export JAVA_HOME=`/usr/libexec/java_home -v 9`
+## export JAVA_HOME=`/usr/libexec/java_home -v 1.8e`
+export JAVA_HOME=`/usr/libexec/java_home -v 11`
