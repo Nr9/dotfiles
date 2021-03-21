7920a29824cf181b6cc70f1d723ab829c0b22e2a
Julien Marcil <julien.marcil@ingeno.ca>
2021-03-21 16:18:01 -0400

    java fix

--- java/java.zsh
+++ java/java.zsh
@@ -1,2 +1,4 @@
-## export JAVA_HOME=`/usr/libexec/java_home -v 1.8e`
-export JAVA_HOME=`/usr/libexec/java_home -v 11`
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/openjdk.jdk/Contents/Home"
+##export JAVA_HOME="/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home"
+##export JAVA_HOME=`/usr/libexec/java_home -v 1.8e`
+##export JAVA_HOME=`/usr/libexec/java_home -v 15`
